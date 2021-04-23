45d281496bd80dcea74c2be102100bb9fc73c86b
GostyuninVA <48084264+GostyuninVA@users.noreply.github.com>
2021-04-23 17:40:29 +0300

    Initial commit

---
+++ Assets/Scripts/Slicer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using BzKovSoft.ObjectSlicer;
+using BzKovSoft.ObjectSlicerSamples;
+
+public class Slicer : MonoBehaviour
+{
+    [SerializeField] private GameObject _blade;
+    [SerializeField] private float _duration;
+    [SerializeField] private float _offsetY;
+
+    private BzKnife _knife;
+
+    private Vector3 _knifeStartPosition;
+    private bool _isKnifeInTheStartPosition;
+
+    private void Start()
+    {
+        _knife = _blade.GetComponentInChildren<BzKnife>();
+
+        _knifeStartPosition = _blade.transform.position;
+        _isKnifeInTheStartPosition = true;
+    }
+
+    private void Update()
+    {
+        _isKnifeInTheStartPosition = _knifeStartPosition == _blade.transform.position;
+
+        if(Input.GetMouseButtonDown(0) && _isKnifeInTheStartPosition)
+        {
+            _knife.BeginNewSlice();
+
+            _blade.transform.DOMoveY(_blade.transform.position.y - _offsetY, _duration).SetLoops(2, LoopType.Yoyo);
+        }
+    }
+
+
+}
